6abd99291ef9cabd8d070233baa7dde76deb5f87
kevin <kevleone10@gmail.com>
2019-11-05 15:37:33 +0300

    thumbnails not showing fix

--- djangonautic/urls.py
+++ djangonautic/urls.py
@@ -2,6 +2,9 @@ from django.contrib import admin
 from django.urls import path
 from django.conf.urls import url,include
 from . import views
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
@@ -9,3 +12,6 @@ urlpatterns = [
     url(r'^about/$',views.about),
     url(r'^$',views.homepage),
 ]
+
+urlpatterns += staticfiles_urlpatterns()
+urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
