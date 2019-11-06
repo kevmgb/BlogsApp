346eda9eb8ce816ffcef98bfa8df305e88f2de49
kevin <kevleone10@gmail.com>
2019-11-06 13:06:04 +0300

    require login

--- articles/views.py
+++ articles/views.py
@@ -4,9 +4,12 @@ from django.http import HttpResponse
 
 # Create your views here.
 
+
+
 def article_list(request):
     articles = Article.objects.all().order_by('date')
     return render(request,'articles/article_list.html', {'articles': articles})
+
 
 
 def article_detail(request,slug):
@@ -14,3 +17,9 @@ def article_detail(request,slug):
 
     article = Article.objects.get(slug=slug)
     return render (request,'articles/article_detail.html', {'article':article})
+
+
+
+def article_create(request):
+    return render(request,'articles/article_create.html')
+
